73817d8bf6f271627efa52b03456ad57793cbcce
victorleon0 <victor.leon.huerta@gmail.com>
2022-05-29 22:34:32 +0100

    CHANGES: style changes

--- pokedex.js
+++ pokedex.js
@@ -3,8 +3,6 @@ const container = document.querySelector(".container");
 const ALL_POKEMONS = [];
 const ALL_POKEMONS_INFO = [];
 
-
-
 const getPokemonsAll = () => {
   return fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
     .then((response) => {
@@ -28,8 +26,11 @@ const getPokemonById = async (pokemonsList) => {
       type: pokemon.types.map((type) => type.type.name),
       image: pokemon.sprites["front_default"],
       imageBack: pokemon.sprites["back_default"],
+      peso: pokemon.weight,
+      altura: pokemon.height,
+      habilidad: pokemon.abilities.map((ability) => ability.ability.name),
     };
-    
+
     ALL_POKEMONS.push(poke);
   }
   return ALL_POKEMONS;
@@ -38,11 +39,6 @@ const getPokemonById = async (pokemonsList) => {
 function createHeader() {
   const header = document.createElement("header");
   header.className = "header";
-
-  const h1 = document.createElement("h1");
-  h1.className = "headerTitle";
-  h1.innerText = "Pokedex";
-  header.appendChild(h1);
 
   const divLogo = document.createElement("div");
   divLogo.classList.add("logoHeader");
@@ -67,14 +63,11 @@ function createHeader() {
   orderButton.classList.add("orderButton");
   orderButton.innerText = "Ordenar";
   divInput.appendChild(orderButton);
-
   
-
   document.body.insertBefore(header, container);
 }
 
 const drawPokemons = (list) => {
-
   pokedex.innerHTML = "";
 
   for (pokemon of list) {
@@ -93,49 +86,52 @@ const drawPokemons = (list) => {
     h3.className = "pokemonName";
 
     const p = document.createElement("p");
-    p.innerText = pokemon.type;
+    p.innerText = "Tipo " + pokemon.type;
     p.className = "type";
 
     const divBack = document.createElement("div");
     divBack.className = "back";
 
+    const habilidadPokemonBack = document.createElement("p");
+    habilidadPokemonBack.innerText = "Habilidades:  " + pokemon.habilidad;
+    habilidadPokemonBack.className = "habilidades";
+
+    const pesoBack = document.createElement("p");
+    pesoBack.innerText = "Peso  " + pokemon.peso + "KG";
+    pesoBack.className = "peso";
+
+    const alturaBack = document.createElement("p");
+    alturaBack.innerText = "Altura  " + pokemon.altura + "M";
+    alturaBack.className = "altura";
+
     const imgBack = document.createElement("img");
     imgBack.className = "imgPokemonBack";
     imgBack.setAttribute("src", pokemon.imageBack);
 
-    const h3Back = document.createElement("h3");
-    h3Back.innerText = pokemon.name;
-    h3Back.className = "pokemonNameBack";
-
-
-    const pBack = document.createElement("p");
-    pBack.innerText = pokemon.type;
-    pBack.className = "descriptionBack";
-
+    //const h3Back = document.createElement("h3");
+    //h3Back.innerText = pokemon.name;
+    //h3Back.className = "pokemonNameBack";
 
     pokedex.appendChild(divPokemon);
     divPokemon.appendChild(div);
     divPokemon.appendChild(divBack);
     div.appendChild(h3);
-    divBack.appendChild(h3Back);
+    //divBack.appendChild(h3Back);
     div.appendChild(img);
     divBack.appendChild(imgBack);
     div.appendChild(p);
-    divBack.appendChild(pBack);
-
+    //divBack.appendChild(pBack);
+    divBack.appendChild(pesoBack);
+    divBack.appendChild(alturaBack);
+    divBack.appendChild(habilidadPokemonBack);
   }
 };
 
-
-
 const searchPokemons = (event) => {
-
   const buscaInput = event.target.value.toLowerCase();
-  const resultPokemon = ALL_POKEMONS.filter(pokemon => {
-
+  const resultPokemon = ALL_POKEMONS.filter((pokemon) => {
     const matchName = pokemon.name.toLowerCase().includes(buscaInput);
-    return matchName
-    
+    return matchName;
   });
   console.log(buscaInput);
 
@@ -143,10 +139,67 @@ const searchPokemons = (event) => {
 };
 
 const eventsListener = () => {
-  document.querySelector(".inputSearch").addEventListener("input", searchPokemons);
-  
+  document
+    .querySelector(".inputSearch")
+    .addEventListener("input", searchPokemons);
 };
 
+function createFooter() {
+  const footer = document.createElement("footer");
+  footer.className = "footer-container";
+  document.body.appendChild(footer);
+
+  const h4Footer = document.createElement("h1");
+  h4Footer.className = "copyright";
+  h4Footer.innerText = "Copyright (c) - Víctor León 2022";
+  footer.appendChild(h4Footer);
+
+  const divLogoFooter = document.createElement("div");
+  divLogoFooter.classList.add("logoFooter-container");
+  footer.appendChild(divLogoFooter);
+
+  const imgFooter = document.createElement("img");
+  imgFooter.classList.add("imgFooter");
+  imgFooter.src = "https://www.freepnglogos.com/uploads/pokemon-logo-png-0.png";
+  imgFooter.alt = "Pokemon Logo Footer";
+  divLogoFooter.appendChild(imgFooter);
+
+  const divSocial = document.createElement("div");
+  divSocial.classList.add("social-container");
+  footer.appendChild(divSocial);
+
+  const logoLinkedin = document.createElement("img");
+  logoLinkedin.classList.add("imgLinkedin");
+  logoLinkedin.src = "https://cdn-icons-png.flaticon.com/512/174/174857.png";
+  logoLinkedin.href = "https://www.linkedin.com/in/victorleon0/";
+  divSocial.appendChild(logoLinkedin);
+
+  const logoFacebook = document.createElement("img");
+  logoFacebook.classList.add("imgFacebook");
+  logoFacebook.src = "https://cdn-icons-png.flaticon.com/512/174/174848.png";
+  logoFacebook.href = "https://www.facebook.com/Victor.Leoon/";
+  divSocial.appendChild(logoFacebook);
+
+  const logoInstagram = document.createElement("img");
+  logoInstagram.classList.add("imgInstagram");
+  logoInstagram.src =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png";
+  logoInstagram.href = "https://www.instagram.com/victorleon_/";
+  divSocial.appendChild(logoInstagram);
+
+  const logoGithub = document.createElement("img");
+  logoGithub.classList.add("imgGithub");
+  logoGithub.src = "https://cdn-icons-png.flaticon.com/512/25/25231.png";
+  logoGithub.href = "https://github.com/victorleon0";
+  divSocial.appendChild(logoGithub);
+
+  const logoEmail = document.createElement("img");
+  logoEmail.classList.add("imgEmail");
+  logoEmail.src =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png";
+  logoEmail.href = "mailto: victor.leon.huerta@gmail.com";
+  divSocial.appendChild(logoEmail);
+}
 
 const initApp = async () => {
   const data = await getPokemonsAll();
@@ -154,10 +207,9 @@ const initApp = async () => {
   createHeader();
   drawPokemons(pokemons);
   eventsListener();
+  createFooter();
 
-  pokemons.forEach((pokemon) => {
-    
-  });
+  pokemons.forEach((pokemon) => {});
 };
 
 initApp();
